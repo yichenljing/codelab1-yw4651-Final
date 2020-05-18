5cc2e3781e1474e907f6a0f4b31e885cb944bf23
yichenljing <40904846+yichenljing@users.noreply.github.com>
2020-05-18 12:18:47 -0400

    fix bugs

--- codelab1-yw4651-Final/Assets/Scripts/Location.cs
+++ codelab1-yw4651-Final/Assets/Scripts/Location.cs
@@ -19,66 +19,71 @@ public class Location : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (GameManager.instance.Currentlocation == 1)
-        {
-            UIlocation.SetActive(true);
-            //    if currentlocation ==1, the description changes to forest detail
-            UIlocation.GetComponent<Text>().text = ("you enter the forest\n You got the power from the nature. \n Power +1 ");
-        
-            
-        }
+        if (UIlocation !=null) {
+            if (GameManager.instance.Currentlocation == 1)
+            {
+                UIlocation.SetActive(true);
+                //    if currentlocation ==1, the description changes to forest detail
+                UIlocation.GetComponent<Text>().text = ("you enter the forest\n You got the power from the nature. \n Power +1 ");
 
-        if (GameManager.instance.Currentlocation == 2)
-        {
-            UIlocation.SetActive(true);
-            UIlocation.GetComponent<Text>().text = ("Consume 2 food \n You enter the Badland.\n \n You consumed more food, but your Power seems to have increased under the harsh conditions of Badland\n\n  Power +1");
-        }
 
-        if (GameManager.instance.Currentlocation == 3)
-        {
-            UIlocation.SetActive(true);
-            UIlocation.GetComponent<Text>().text = ("Consume 1 food \n You enter the Grass area.\n \n nothing happens");
-        }
+            }
 
-        if (GameManager.instance.Currentlocation == 4)
-        {
-            UIlocation.SetActive(true);
-            UIlocation.GetComponent<Text>().text = ("Consume 1 food \n\n  You enter the Mushroom area. \n \n Food+2");
-        }
+            if (GameManager.instance.Currentlocation == 2)
+            {
+                UIlocation.SetActive(true);
+                UIlocation.GetComponent<Text>().text = ("Consume 2 food \n You enter the Badland.\n \n You consumed more food, but your Power seems to have increased under the harsh conditions of Badland\n\n  Power +1");
+            }
 
-        if (GameManager.instance.Currentlocation == 5)
-        {
-            UIlocation.SetActive(true);
-            UIlocation.GetComponent<Text>().text = ("Consume 1 food \n \n An evil Beast blocked the way");
+            if (GameManager.instance.Currentlocation == 3)
+            {
+                UIlocation.SetActive(true);
+                UIlocation.GetComponent<Text>().text = ("Consume 1 food \n You enter the Grass area.\n \n nothing happens");
+            }
 
-            if (attactinfo.activeInHierarchy == false)
+            if (GameManager.instance.Currentlocation == 4)
+            {
+                UIlocation.SetActive(true);
+                UIlocation.GetComponent<Text>().text = ("Consume 1 food \n\n  You enter the Mushroom area. \n \n Food+2");
+            }
+
+            if (GameManager.instance.Currentlocation == 5)
+            {
+                UIlocation.SetActive(true);
+                UIlocation.GetComponent<Text>().text = ("Consume 1 food \n \n An evil Beast blocked the way");
+
+                if (attactinfo.activeInHierarchy == false)
+                {
+
+                    attackButton.SetActive(true);
+
+                }
+                else if (attactinfo.activeInHierarchy == true)
+                {
+                    attackButton.SetActive(false);
+                }
+
+
+            }
+
+            if (GameManager.instance.Currentlocation == 6)
+            {
+                UIlocation.SetActive(true);
+                UIlocation.GetComponent<Text>().text = ("Consume 1 food \n\n  You enter the forest\n\n  You meet an elf in the forest, and you get extra power from him! \n \n Power+2");
+            }
+
+            if (GameManager.instance.Currentlocation == 7)
             {
 
-                attackButton.SetActive(true);
+                UIlocation.SetActive(true);
 
+                UIlocation.GetComponent<Text>().text = ("Consume 1 food \n\n s You enter the Grass area.\n\n  You find some berries over the grass!  \n \n Food+1");
             }
-            else if(attactinfo.activeInHierarchy == true) {
-                attackButton.SetActive(false);
-            }
-
-
         }
 
-        if (GameManager.instance.Currentlocation == 6)
-        {
-            UIlocation.SetActive(true);
-            UIlocation.GetComponent<Text>().text = ("Consume 1 food \n\n  You enter the forest\n\n  You meet an elf in the forest, and you get extra power from him! \n \n Power+2");
-        }
 
-        if (GameManager.instance.Currentlocation == 7)
-        {
 
-            UIlocation.SetActive(true);
-            
-            UIlocation.GetComponent<Text>().text = ("Consume 1 food \n\n s You enter the Grass area.\n\n  You find some berries over the grass!  \n \n Food+1");
-        }
     }
-
 
 
 
